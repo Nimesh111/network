eee590ad2f56f101ebfdb3cc26561a696072e877
Nimesh111 <36208868+Nimesh111@users.noreply.github.com>
2018-04-19 17:44:23 +0530

    Update CheckTelent.py

--- CheckTelent.py
+++ CheckTelent.py
@@ -3,23 +3,26 @@ import telnetlib
 import sys
 
 def telentcheck():
-	# Enter your host IP address here..
-	Host=""
-	username=raw_input("Enter Your Username:")
-	password= getpass.getpass()
-	
-	tn=telnetlib.Telnet(Host)
-	
-	tn.read_until("Username: ")
-	tn.write(username + "\n")
-	if password:
-		tn.read_until("Password: ")
-		tn.write(password + "\n")
-	
-	tn.write("show ip int br\n")
-	tn.write("exit\n")
-	
-	print tn.read_all()
+	try:
+		# Host IP address will be IP address of device to which you want to connect. 
+		Host="127.0.0.1"
+		username=raw_input("Enter Your Username:")
+		password= getpass.getpass()
+		
+		tn=telnetlib.Telnet(Host)
+		
+		tn.read_until("Username: ")
+		tn.write(username + "\n")
+		if password:
+			tn.read_until("Password: ")
+			tn.write(password + "\n")
+		
+		tn.write("show ip int br\n")
+		tn.write("exit\n")
+		
+		print tn.read_all()
+	except:
+		print "Connection cannot be established."
 
-if __name__="__main__"
+if __name__=="__main__":
 	telentcheck()
